ba897166fe249176b81ab5541c9e5724ef084a29
Lucky Emmanuel <luckyogala652@gmail.com>
2026-01-04 22:22:17 +0100

    Adds Paystack

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,11 +1,33 @@
 import React, { useContext } from 'react';
 import { CartContext } from '../context/CartContext';
 import '../cart.css';
+import { PaystackButton } from "react-paystack";
+
+const publicKey = "pk_live_6eafbca693efa8a357d54482f23083003eba5084"; //replace with paystack public key
 
 function CartPage() {
   const { cart, removeFromCart } = useContext(CartContext);
 
   const total = cart.reduce((sum, item) => sum + item.price, 0);
+  const verifyPayment = async (reference) => {
+    const res = await
+    fetch(`https://ecommerce-backend-ejce.onrender.com/verify/${reference}`);
+    const data = await res.json();
+
+    if (data.data.status === "success") {
+      alert("Payment Successful");
+    }
+  };
+  const email = "luckyogala652@gmail.com";
+
+  const componentProps = {
+    email,
+    amount: total * 100, publicKey,
+    text: "Pay Now",
+    onSuccess: (response) => 
+      verifyPayment(response.reference),
+    onClose: () => alert("Payment cancelled"),
+  };
 
   return (
     <div className="cart-container">
@@ -32,6 +54,7 @@ function CartPage() {
               </button>
             </div>
           ))}
+          <PaystackButton {...componentProps} className="pay-btn" />
 
           <div className="cart-summary">
             <h3>Total: ${total.toFixed(2)}</h3>
